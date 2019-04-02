5b8c1a443ddb16446e53557d70e598dc52a60b7b
Minami <minami-git@minami-vr.sakura.ne.jp>
2019-04-02 19:13:35 +0900

    change to output in main thread

--- multithread.go
+++ multithread.go
@@ -1,35 +1,55 @@
 package main
 
 import (
-	"fmt"
 	"math"
-	"sync"
+	"os"
+	"runtime"
+	"strconv"
+)
+
+const (
+	_  = iota
+	KB = 1 << (10 * iota)
+	MB = 1 << (10 * iota)
+)
+
+const (
+	chanSize = KB
+	bufSize  = MB
 )
 
 func main() {
-	thread := runtime.NumCPU()
-	wg := &sync.WaitGroup{}
+	thread := uint64(runtime.NumCPU())
+	c := make(chan uint64, chanSize)
 	for i := uint64(3); i < uint64(2*thread+3); i += 2 {
-		go prime(i, thread, wg)
+		go prime(i, thread, c)
 	}
-	wg.Wait()
+	buf := make([]byte, 0)
+	for {
+		buf = append(buf, strconv.FormatUint(<-c, 10)...)
+		buf = append(buf, '\n')
+		if len(buf) > bufSize {
+			os.Stdout.Write(buf)
+			buf = make([]byte, 0)
+		}
+	}
 }
 
-func prime(n uint64, thread int, wg *sync.WaitGroup) {
-	wg.Add(1)
+func prime(n uint64, thread uint64, c chan uint64) {
 	increment := uint64(2 * thread)
-	max := math.MaxUint64 - increment
+	max := uint64(math.MaxUint64 - increment)
 	for i := n; i < max; i += increment {
-		isPrime := true
-		for j := uint64(3); j*j <= i; j += uint64(2) {
-			if i%j == 0 {
-				isPrime = false
-				break
-			}
-		}
-		if isPrime {
-			fmt.Println(i)
+		if isPrime(i) {
+			c <- i
 		}
 	}
-	wg.Done()
+}
+
+func isPrime(i uint64) bool {
+	for j := uint64(3); j*j <= i; j += uint64(2) {
+		if i%j == 0 {
+			return false
+		}
+	}
+	return true
 }
